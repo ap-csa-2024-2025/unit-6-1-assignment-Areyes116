e91a6141bb0ecb7b9b641ac73f907d8e4171c497
Areyes116 <adrian.reyes2006@icloud.com>
2025-01-17 03:17:09 +0000

    Arise

--- Main.java
+++ Main.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
 
-public class Main
+public class ArrayProblems
 {
-  public static void main(String[] args)
-  {
-    // Problem 1 - Create a scanner and an array of size 3
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
 
-    // Problem 2
-    // Use the scanner to get user input and give it to the method
+        // Problem 1
+        double[] values = new double[3];
+        System.out.println("Enter three double values:");
+        values[0] = scanner.nextDouble();
+        values[1] = scanner.nextDouble();
+        values[2] = scanner.nextDouble();
 
-    int N = /* scan.nextInt() */;
-    int[] output = sumsOfNums(N);
-  }
+        double sum = values[0] + values[1] + values[2];
 
-  public static int[] sumsOfNums(int input)
-  {
-    // Implement Problem 2 here
-    return 0;
-  }
+        System.out.println("Array contents: " + values[0] + ", " + values[1] + ", " + values[2]);
+        System.out.println("Sum of array: " + sum);
+
+        // Problem 2
+        System.out.print("Enter a positive number N: ");
+        int N = scanner.nextInt();
+        
+        if (N < 0)
+        {
+            System.out.println("Please enter a positive number.");
+        }
+        else
+        {
+            int[] sums = new int[N];
+            sums[0] = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                sums[i] = sums[i - 1] + i;
+            }
+
+            System.out.print("Summed array: {");
+            for (int i = 0; i < N; i++)
+            {
+                System.out.print(sums[i]);
+                if (i < N - 1) System.out.print(", ");
+            }
+            System.out.println("}");
+        }
+
+        scanner.close();
+    }
 }
